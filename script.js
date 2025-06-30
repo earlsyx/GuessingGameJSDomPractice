40a71f53aca6c02357d298fd5e8a7d7bf1d2bcd0
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-01 07:56:32 +0800

    Add functionality for again button

--- script.js
+++ script.js
@@ -17,8 +17,7 @@ console.log(document.querySelector('.guess').value); // input field
 function expression remember?
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20; //state variable - part of application state - meaning all the data that is relevant to the application
 
@@ -32,6 +31,8 @@ document.querySelector('.check').addEventListener('click', function () {
 
     // When player wins
   } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+
     document.querySelector('.message').textContent = '🎉 Correct Number!';
     document.querySelector('body').style.backgroundColor = '#60b347';
 
@@ -58,4 +59,16 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  //rest
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').style.width = '15rem';
 });
