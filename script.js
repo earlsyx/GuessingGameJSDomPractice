621eafe533b4b8c95b30b151631d1d5d5a730a68
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-01 08:00:30 +0800

    Implementing High Score

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20; //state variable - part of application state - meaning all the data that is relevant to the application
 
+let highscore = 0;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -37,6 +39,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem ';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // Guess to HIgh
   } else if (guess > secretNumber) {
